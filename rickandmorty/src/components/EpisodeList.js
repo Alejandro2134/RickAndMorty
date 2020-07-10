d8d951559384aadccd6bb394deddf7f498956396
Alejandro2134 <zapatamolinaalejandro@gmail.com>
2020-07-09 21:44:31 -0500

    Changes

--- rickandmorty/src/components/EpisodeList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const EpisodeList = () => {
-    return(
-        <h1>EpisodeList</h1>
-    )
-}
-
-export default EpisodeList;
--- rickandmorty/src/components/LocationList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LocationList = () => {
-    return(
-        <h1>LocationList</h1>
-    )
-}
-
-export default LocationList;
